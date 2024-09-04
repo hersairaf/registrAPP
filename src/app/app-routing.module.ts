5be87de53bd6e2387fc3d89e63722374d75e3cae
Hernan Farias <her.farias@duocuc.cl>
2024-09-04 13:40:44 -0400

    Creación de la vista login
    Se creó la vista Login y quedó configurada para que quede como inicio predeterminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
+  // meter otras rutas acá 
 ];
 
 @NgModule({
